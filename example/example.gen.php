1a0d7365770e94b81723aa80c3c18cc90a06f785
Christian Mayer <thefox21at@gmail.com>
2014-06-02 22:46:27 +0200

    Example gen.

--- example/example.gen.php
+++ example/example.gen.php
@@ -6,22 +6,30 @@ require_once __DIR__.'/../bootstrap.php';
 
 use Symfony\Component\Yaml\Yaml;
 
-exit(1);
-
 $settings = array(
-	
 	'vars' => array(
 		'Title' => 'my example page',
-		'CustomCSS' => '<!-- my custom //-->',
+		'CustomCSS' => 'my custom',
+		'Description' => 'my description',
 		'MetaDescription' => 'my meta description',
-		'if:Show Archive' => 0,
+		'if:AskEnabled' => 1,
+		'AskLabel' => 'my ask label',
+		'lang:Newer posts' => 'Newer Posts',
+		'lang:Older posts' => 'Older Posts',
+		'text:Google Analytics ID' => 1234,
 	),
-	
 	'posts' => array(
 		array('type' => 'Text', 'Title' => 'test1', 'Permalink' => '/post/1', 'content' => 'test1.content'),
+		array('type' => 'text', 'permalink' => '?type=post&index=1', 'title' => 'title_test1', 'body' => 'test1.body'),
+		array('type' => 'text', 'body' => 'test2.body'),
+		array('type' => 'text', 'title' => 'title_test3'),
+		array('type' => 'link', 'url' => 'http://fox21.at', 'name' => 'link_name1', 'target' => 'target="_blank"', 'description' => 'this is my description'),
+		array('type' => 'link', 'url' => 'http://www.fox21.at', 'name' => 'link_name2'),
+		array('type' => 'link', 'url' => 'http://www.fox21.at', 'name' => 'link_name2'),
+		array('type' => 'photo', 'url' => 'https://farm3.staticflickr.com/2882/10004722973_1774a72748.jpg', 'link' => 'https://en.wikipedia.org/wiki/Halloumi', 'alt' => 'my alt text', 'caption' => 'my caption text'),
+		array('type' => 'photo', 'url' => 'https://farm3.staticflickr.com/2882/10004722973_1774a72748.jpg', 'alt' => 'my alt text'),
 	),
-	
-	'postsPerPage' => 15,
+	'postsPerPage' => 50,
 );
 
 file_put_contents(__DIR__.'/example.settings.yml', Yaml::dump($settings));
